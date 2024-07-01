ebf86400d20289cc4435bb3f8deb82e83776a7f3
Justus78 <stars787878@aol.com>
2024-06-30 20:49:03 -0400

    Add scope in program.cs for repos and interfaces

--- SoftballStats/Program.cs
+++ SoftballStats/Program.cs
@@ -1,15 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using SoftballStats.Data;
+using SoftballStats.Repositories;
+using SoftballStats.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// add the db context connection string
 builder.Services.AddDbContext<StatContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+// add scope for the interfaces and repositories
+builder.Services.AddScoped<IPlayer, PlayerRepository>();
+builder.Services.AddScoped<ITeam, TeamRepository>();
+builder.Services.AddScoped<IStats, StatRepository>();
 
 var app = builder.Build();
 
