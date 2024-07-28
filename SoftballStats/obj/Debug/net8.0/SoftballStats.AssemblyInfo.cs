b88e0e8a35836dd215d3b784a5d6d22e8641baf9
Justus78 <stars787878@aol.com>
2024-07-28 16:58:21 -0400

    added styles for Register page

--- SoftballStats/obj/Debug/net8.0/SoftballStats.AssemblyInfo.cs
+++ SoftballStats/obj/Debug/net8.0/SoftballStats.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftballStats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5c66f588dfaaebf88efb4da2689c771d3e99af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ae94c8a174e03e7a85ccc6d66febfbced899d75")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftballStats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftballStats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
