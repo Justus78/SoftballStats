2cc8e8ac2e08c68cbae402ba7578ae8173fd0488
Justus78 <stars787878@aol.com>
2024-08-25 16:29:19 -0400

    added new git ignore file

--- SoftballStats/obj/Debug/net8.0/SoftballStats.AssemblyInfo.cs
+++ SoftballStats/obj/Debug/net8.0/SoftballStats.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftballStats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88e0e8a35836dd215d3b784a5d6d22e8641baf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a945357a39068aaaaac826c685e3008631ad7611")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftballStats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftballStats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
